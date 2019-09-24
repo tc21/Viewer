3f4a730649a36c510e60a2daf1e6d5384e290ad9
Tianyi Cao <tc21@live.com>
2019-09-23 23:47:27 -0700

    Added a view model

--- Viewer/MainWindow.xaml.cs
+++ Viewer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,67 +16,34 @@ using System.Windows.Navigation;
 
 namespace Viewer {
     public partial class MainWindow : Window {
-        private readonly List<string> images = new List<string>();
 
-        private int _currentImageIndex;
-        public int CurrentImageIndex {
-            get => _currentImageIndex;
-            private set {
-                if (images.Count == 0) {
-                    return;
-                }
-
-                _currentImageIndex = ActualIndex(value);
-                MainImage.Source = LoadImage(_currentImageIndex);
-                UpdateTitle();
-            }
-        }
+        // Expose view model properties to the public API here
 
         // When a list of objects is assigned to CustomContextMenuItems, it build a new context menu.
         // Assign null to CustomContextMenuItems in object initialization to ensure the menu does property get built.
-        private IList<object> _customContextMenuItems;
+
+
+        private IList<object> customContextMenuItems;
         public IList<object> CustomContextMenuItems {
-            private get => _customContextMenuItems;
             set {
-                if (this.ContextMenu == null) {
-                    this.ContextMenu = new ContextMenu();
+                if (this.customContextMenuItems == value) {
+                    return;
                 }
 
-                var menuItems = this.ContextMenu.Items;
-
-                menuItems.Clear();
-
-                var upper = (object[])this.FindResource("UpperContextMenu");
-                var lower = (object[])this.FindResource("LowerContextMenu");
-
-                foreach (var item in upper) {
-                    menuItems.Add(item);
-                }
-                menuItems.Add(new Separator());
-
-                if (value != null) {
-                    foreach (var item in value) {
-                        menuItems.Add(item);
-                    }
-                    menuItems.Add(new Separator());
-                }
-
-                foreach (var item in lower) {
-                    menuItems.Add(item);
-                }
-
-                this._customContextMenuItems = value;
+                this.customContextMenuItems = value;
+                BuildContextMenu(value);
             }
         }
 
-        // First of many apis for embedding in other programs
-        public string CurrentImage => images[CurrentImageIndex];
+        public string CurrentImage => this.viewModel.CurrentImage;
+        public int CurrentImageIndex => this.viewModel.CurrentImageIndex;
 
-        private string title = "Viewer";
-        private readonly string[] imageExtensions = { ".bmp", ".gif", ".ico", ".jpeg", ".jpg", ".png", ".tiff", ".tif" };
+        private readonly ViewModel viewModel = new ViewModel();
 
         public MainWindow(string[] files) {
             InitializeComponent();
+
+            this.DataContext = this.viewModel;
 
             Top = Properties.Settings.Default.Top;
             Left = Properties.Settings.Default.Left;
@@ -92,128 +60,81 @@ namespace Viewer {
             }
 
             // args come from App, which is the command line arguments
-            if (files != null && files.Length > 0) {
-                ProcessFiles(files);
-            }
+            this.viewModel.LoadImages(files);
         }
 
         /* returns whether the index actually exists */
         public bool GoToIndex(int index) {
-            if (ActualIndex(index) != index) {
+            if (!(0 <= index && index < viewModel.Images.Count)) {
                 return false;
             }
 
-            CurrentImageIndex = index;
+            viewModel.CurrentImageIndex = index;
             return true;
         }
 
-        private BitmapImage LoadImage(int index) {
-            var file = images[ActualIndex(index)];
-            var image = new BitmapImage();
-            image.BeginInit();
-            image.UriSource = new Uri(file);
-            image.EndInit();
-            return image;
+        public void Open(IEnumerable<string> paths) {
+            if (paths.Count() == 1) {
+                var path = paths.First();
+                if (Directory.Exists(path)) {  // "file" is actually a directory!
+                    this.viewModel.LoadImages(Directory.GetFiles(path));
+                } else {
+                    // we don't just load one file, but load the containing directory and navigate to this file.
+                    var directoryName = Path.GetDirectoryName(path);
+                    var directoryFiles = Directory.GetFiles(directoryName);
+                    this.viewModel.LoadImages(directoryFiles);
+                    this.viewModel.CurrentImageIndex = this.viewModel.Images.IndexOf(path);
+                }
+            } else {
+                this.viewModel.LoadImages(paths);
+            }
         }
 
-        private int ActualIndex(int index) {
-            return (index + images.Count) % images.Count;
+        private void BuildContextMenu(IEnumerable<object> customContextMenuItems) {
+            if (this.ContextMenu == null) {
+                this.ContextMenu = new ContextMenu();
+            }
+
+            var menuItems = this.ContextMenu.Items;
+
+            menuItems.Clear();
+
+            var upper = (object[])this.FindResource("UpperContextMenu");
+            var lower = (object[])this.FindResource("LowerContextMenu");
+
+            foreach (var item in upper) {
+                menuItems.Add(item);
+            }
+            menuItems.Add(new Separator());
+
+            if (customContextMenuItems != null) {
+                foreach (var item in customContextMenuItems) {
+                    menuItems.Add(item);
+                }
+                menuItems.Add(new Separator());
+            }
+
+            foreach (var item in lower) {
+                menuItems.Add(item);
+            }
         }
 
         private void Window_Drop(object sender, DragEventArgs e) {
             if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
-                images.Clear();
                 var files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                ProcessFiles(files);
-            }
-        }
-
-        private void ProcessFiles(string[] files) {
-            if (files.Length == 1) {
-                var file = files[0];
-                if (Directory.Exists(file)) {
-                    OpenFolder(file);
-                } else {
-                    OpenContainingFolder(file);
-                }
-            } else {
-                OpenFiles(files);
-            }
-        }
-
-        private void OpenContainingFolder(string path) {
-            var directoryName = Path.GetDirectoryName(path);
-            var files = Directory.GetFiles(directoryName);
-            LoadFiles(files);
-
-            if (images.Count > 0) {
-                title = " (" + directoryName + ")";
-                CurrentImageIndex = images.IndexOf(path);
-            }
-        }
-
-        private void OpenFiles(string[] files) {
-            LoadFiles(files);
-
-            if (images.Count > 0) {
-                title = " (" + images.Count.ToString() + " files loaded)";
-                CurrentImageIndex = 0;
-            }
-        }
-
-        private void OpenFolder(string path) {
-            var files = Directory.GetFiles(path);
-            LoadFiles(files);
-
-            if (images.Count > 0) {
-                title = " (" + path + ")";
-                CurrentImageIndex = 0;
-            }
-
-            // This is very dangerous!!! For personal use only, I guess.
-            var dirs = Directory.GetDirectories(path);
-            foreach (var dir in dirs) {
-                Properties.Settings.Default.Top += 15;
-                Properties.Settings.Default.Left += 15;
-                var newWindow = new MainWindow(new string[] { dir });
-                newWindow.Show();
-            }
-        }
-
-        private bool IsImage(string path) {
-            return imageExtensions.Contains(Path.GetExtension(path).ToLower());
-        }
-
-        private void LoadFiles(IEnumerable<string> files) {
-            foreach (var file in files) {
-                if (!IsImage(file)) {
-                    continue;
-                }
-
-                images.Add(file);
-            }
-
-            if (images.Count == 0) {
-                Title = "No files loaded";
-                MainImage.Source = null;
+                Open(files);
             }
         }
 
         private void Window_MouseWheel(object sender, MouseWheelEventArgs e) {
-            if (e.Delta > 0)  // This means scrolled up
-               {
-                CurrentImageIndex -= 1;
+            if (e.Delta > 0) { // This means scrolled up
+                this.viewModel.CurrentImageIndex -= 1;
             } else {
-                CurrentImageIndex += 1;
+                this.viewModel.CurrentImageIndex += 1;
             }
         }
 
-        private void UpdateTitle() {
-            var filename = Path.GetFileName(images[CurrentImageIndex]);
-            Title = filename + title;
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+        private void Window_Closing(object sender, CancelEventArgs e) {
             if (WindowState == WindowState.Maximized) {
                 Properties.Settings.Default.Top = RestoreBounds.Top;
                 Properties.Settings.Default.Left = RestoreBounds.Left;
@@ -228,15 +149,15 @@ namespace Viewer {
         }
 
         private void Window_MouseUp(object sender, MouseButtonEventArgs e) {
-            CurrentImageIndex += 1;
+            this.viewModel.CurrentImageIndex += 1;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e) {
             if (e.Key == Key.Right || e.Key == Key.Down) {
-                CurrentImageIndex += 1;
+                this.viewModel.CurrentImageIndex += 1;
             }
             if (e.Key == Key.Left || e.Key == Key.Up) {
-                CurrentImageIndex -= 1;
+                this.viewModel.CurrentImageIndex -= 1;
             }
         }
     }
---
+++ Viewer/ViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Viewer {
+    class ViewModel : INotifyPropertyChanged {
+
+        public readonly string[] ImageExtensions = {
+            ".bmp", ".gif", ".heic", ".heif", ".j2k", ".jfi", ".jfif", ".jif", ".jp2", ".jpe", ".jpeg", ".jpf",
+            ".jpg", ".jpm", ".jpx", ".mj2", ".png", ".tif", ".tiff", ".webp"
+        };
+
+        // if you set this directly, make sure all items are valid images
+        private List<string> images = new List<string>();
+        public List<string> Images {
+            get => images;
+            set {
+                if (images == value) {
+                    return;
+                }
+
+                images = value;
+                CurrentImageIndex = 0;  // has the side effect of the NotifyPropertyChanges in CurrentImage::get
+            }
+        }
+
+
+        private int currentImageIndex;
+        public int CurrentImageIndex {
+            get => currentImageIndex;
+            set {
+                if (Images.Count == 0 || currentImageIndex == value) {
+                    return;
+                }
+
+                currentImageIndex = ActualIndex(value);
+                NotifyPropertyChanged(nameof(CurrentImageSource));
+                NotifyPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string CurrentImage => Images[CurrentImageIndex];
+
+
+        public string Title {
+            get {
+                var title = "No files loaded";
+
+                if (Images.Count > 0) {
+                    title = string.Format("{0} ({1}/{2} Files)", Path.GetFileName(CurrentImage), CurrentImageIndex + 1, Images.Count);
+                }
+
+                return title + " - Viewer";
+            }
+        }
+
+        public BitmapImage CurrentImageSource => GetBitmapImage(CurrentImageIndex);
+
+
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void LoadImages(IEnumerable<string> files) {
+            var images = new List<string>();
+
+            foreach (var file in files) {
+                if (IsImage(file)) {
+                    images.Add(file);
+                }
+            }
+
+            Images = images;
+        }
+
+        private int ActualIndex(int index) {
+            return (index + images.Count) % images.Count;
+        }
+
+        // note: index must be valid!
+        private BitmapImage GetBitmapImage(int index) {
+            var file = images[index];
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(file);
+            image.EndInit();
+            return image;
+        }
+
+        private bool IsImage(string path) {
+            return ImageExtensions.Contains(Path.GetExtension(path).ToLower());
+        }
+    }
+}
